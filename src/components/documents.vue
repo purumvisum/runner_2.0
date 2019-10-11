ba23c2f28eeedd60c4187461efd7bee2f3628a4c
Daria <purumvisum@gmail.com>
2019-10-11 16:28:41 +0200

    add search ui component

--- src/components/documents.vue
+++ src/components/documents.vue
@@ -1,13 +1,26 @@
 <template>
-  <v-container height="100%">
-    <v-layout
-      text-center
-      wrap
-    >
-      <add-file-group></add-file-group>
-      <files-groups></files-groups>
-    </v-layout>
-  </v-container>
+    <v-container height="100%">
+        <v-layout text-center wrap>
+            <add-file-group></add-file-group>
+            <v-tooltip top>
+                <template v-slot:activator="{ on }">
+                    <div class="mx-auto" v-on="on" :style="{
+                        position: 'absolute',
+                        width: '42%',
+                        top: '40px'
+                    }">
+                        <v-text-field
+                        color='green'
+                        hide-details
+                        prepend-icon="mdi-magnify"
+                        single-line></v-text-field>
+                    </div>
+                </template>
+                <span>Search by Title</span>
+            </v-tooltip>
+            <files-groups></files-groups>
+        </v-layout>
+    </v-container>
 </template>
 
 <script>
